0d90563282203cfb33adf372db7b577f646a2c01
yazdipour <shahriar.yazdipour@hotmail.com>
2018-06-12 10:02:03 +0430

    first commit

---
+++ Windos/View/MSToDoPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Windows.UI.Xaml.Controls;
+
+namespace Windos.View
+{
+    public sealed partial class MSToDoPage : Page
+    {
+        List<string> NavLinks=new List<string>() { "Navigation" , "Navigation" , "Navigation" };
+        public MSToDoPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ Windos/View/MainPage.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace Windos.View
+{
+    public sealed partial class MainPage : Page
+    {
+        List<NavButton> navButtons = new List<NavButton>()
+        {
+            new NavButton()
+            {
+                Title="File Explorer",
+                PageType=typeof(FileExplorerPage),
+                Bg="#746BC4",
+                Icon=""
+            },
+            new NavButton()
+            {
+                Title="Calculator",
+                PageType=typeof(CacheMode),
+                Bg="#746BC4",
+                Icon=""
+            },
+            new NavButton()
+            {
+                Title="Spotify",
+                PageType=typeof(GroovePage),
+                Bg="#ff0000",
+                Icon=""
+            },
+            new NavButton()
+            {
+                Title="People",
+                PageType=typeof(PeoplePage),
+                Bg="#746BC4",
+                Icon=""
+            },
+            new NavButton()
+            {
+                Title="Skype",
+                PageType=typeof(SkypePage),
+                Bg="#ff0000",
+                Icon=""
+            },
+            new NavButton()
+            {
+                Title="Microsoft ToDo",
+                PageType=typeof(MSToDoPage),
+                Bg="#ff0000",
+                Icon=""
+            }
+        };
+        public MainPage()
+        {
+            this.InitializeComponent();
+            var h = new Helpers.UIHandler();
+            h.TitleBarVisiblity(false, null);
+            h.TitleBarButton_TranparentBackground(false);
+        }
+
+        private void ListView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            var btn = e.ClickedItem as NavButton;
+            Frame.Navigate(btn.PageType);
+        }
+    }
+
+    internal class NavButton
+    {
+        public string Title { get; set; }
+        public string Icon { get; set; }
+        public string Bg { get; set; }
+        public Type PageType;
+    }
+}
