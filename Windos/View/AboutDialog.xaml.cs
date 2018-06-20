6af9e01b046b05ae5ebb9743210a7389ff84b143
yazdipour <shahriar.yazdipour@outlook.com>
2018-06-20 12:04:10 +0430

    New AppIcon

---
+++ Windos/View/AboutDialog.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Content Dialog item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Windos.View
+{
+    public sealed partial class AboutDialog : ContentDialog
+    {
+        public AboutDialog()
+        {
+            this.InitializeComponent();
+            Microsoft.AppCenter.Analytics.Analytics.TrackEvent("Visit AboutPage");
+            Title = "Wind OS";
+        }
+
+        private async void RatingControl_Tapped(object sender, Windows.UI.Xaml.Input.TappedRoutedEventArgs e)
+        {
+            await Windows.System.Launcher.LaunchUriAsync(new Uri("ms-windows-store:REVIEW?PFN=5055881C-9F85-4877-AEA1-AF64140AD34D"));
+        }
+    }
+}
