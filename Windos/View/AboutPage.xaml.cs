a3d87752e70f7f7a28e267b188f9ede346faf8fc
Shahriar <sh.yazdipour@hotmail.com>
2018-06-23 08:51:58 +0430

    Fix Links in Markdown Viewer

--- Windos/View/AboutPage.xaml.cs
+++ Windos/View/AboutPage.xaml.cs
@@ -38,6 +38,9 @@ namespace Windos.View
                 MarkdownText.Width = MarkdownScrollViewer.ActualWidth;
             };
         }
+
+        private string _prefix = "https://github.com/yazdipour/WindOs/raw/master/";
+
         private void GoHome_Click(object sender, RoutedEventArgs e) => Frame.GoBack();
 
         private async void Github_Click(object sender, RoutedEventArgs e)
@@ -47,5 +50,7 @@ namespace Windos.View
         private async void Store_Click(object sender, RoutedEventArgs e)
             => await Windows.System.Launcher.LaunchUriAsync(new Uri("ms-windows-store:REVIEW?PFN=5055881C-9F85-4877-AEA1-AF64140AD34D"));
 
+        private async void MarkdownText_LinkClicked(object sender, Microsoft.Toolkit.Uwp.UI.Controls.LinkClickedEventArgs e)
+            => await Windows.System.Launcher.LaunchUriAsync(new Uri(e.Link.Replace("./",_prefix)));
     }
 }
